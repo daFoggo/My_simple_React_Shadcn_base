41bae770e0f3834d82a3c490d993517d6064c4b0
daFoggo <ntgiang141105@gmail.com>
2024-10-25 03:46:02 +0700

    modify Rootlayout

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,7 +1,26 @@
+import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
+
 const RootLayout = () => {
-
-  return <div>RootLayout</div>;
-
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="z-10">
+      </header>
+      <main className="flex-grow">
+        <Outlet />
+        <Toaster
+          toastOptions={{
+            style: {
+              color: "#4893f4"
+            },
+          }}
+          position="top-center"
+        />
+      </main>
+      <footer className="z-10">
+      </footer>
+    </div>
+  );
 };
 
 export default RootLayout;
